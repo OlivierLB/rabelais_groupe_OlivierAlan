e7161ef184b02a2e67eb88e64f0f5baa094db26f
alan <alan@192.168.1.11>
2015-11-23 14:04:57 +0100

    Première version test

---
+++ src/gestioninscription/gestionRentabilite.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestioninscription;
+
+import gestionRentabilite.*;
+import gestioninscription.FenInscription;
+
+/**
+ *
+ * @author alan
+ */
+public class gestionRentabilite {
+    
+    public static void main(String[] args)
+    {
+        FenInscription f1 = new FenInscription();
+        f1.setSize(1200, 600);
+        f1.setVisible(true);
+        
+    }
+}
